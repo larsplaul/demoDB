f1e99be57d0d9078bf087c4ffe50b0496f657748
Bruger 1 <larsesspam@gmail.com>
2014-01-23 11:53:06 +0100

    Initial Commit

---
+++ src/java/Scripts/scriptInitData.sql
@@ -0,0 +1,3 @@
+
+insert into Person  (ID,FIRSTNAME,LASTNAME)
+VALUES ( 1,'Peter', 'Olsen'),(2,'Jan','Hansen');
---
+++ src/java/entity/Person.java
@@ -0,0 +1,83 @@
+/*
+ */
+
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+/**
+ * @author Lars Mortensen
+ */
+@Entity
+public class Person implements Serializable {
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "myGenerator")
+  @SequenceGenerator(name="myGenerator",sequenceName = "Person_SQ",initialValue = 1000,allocationSize = 1)
+  private Long id;
+  private String firstName;
+  private String lastName;
+
+  public Person() {
+  }
+
+  public Person(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash += (id != null ? id.hashCode() : 0);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    // TODO: Warning - this method won't work in the case the id fields are not set
+    if (!(object instanceof Person)) {
+      return false;
+    }
+    Person other = (Person) object;
+    if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "entity.Person[ id=" + id + " ]";
+  }
+
+}
